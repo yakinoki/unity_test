3eb932a6741c20b3a001534cb14ea47248a5c5aa
NAOYA\bundl <yakinoki>
2023-08-18 23:26:25 +0900

    update_code

--- My project/Assets/Images/PlayerController.cs
+++ My project/Assets/Images/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rbody;
+    float axisH = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        axisH = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rbody.velocity = new Vector2(axisH * 3.0f, rbody.velocity.y);
     }
 }
