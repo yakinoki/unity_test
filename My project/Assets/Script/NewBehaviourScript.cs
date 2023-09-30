d52bb951d3cc90ec6380da7e4a09bb49f66f77e7
NAOYA\bundl <yakinoki>
2023-09-30 17:03:22 +0900

    update_code

--- My project/Assets/Script/NewBehaviourScript.cs
+++ My project/Assets/Script/NewBehaviourScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float leftLimit = 0.0f;
+    public float rightLimit = 0.0f;
+    public float topLimit = 0.0f;
+    public float bottomLimit = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
